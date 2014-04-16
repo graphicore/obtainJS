4993feeb56d9afaa431c52e0e8cfdf7c260576a0
Lasse Fister <commander@graphicore.de>
2014-04-16 04:24:17 +0200

    using Promises for getters and as API

--- tests/obtain.js
+++ tests/obtain.js
@@ -13,16 +13,47 @@ define([
     registerSuite({
         name: 'obtain',
 
-        echo_s: function () {
+        echo_sync: function () {
             var input = 'hello echo';
             assert.strictEqual(echo_job(false, input), input, 'Result should be echod input.');
         }
-      , echo_a: function() {
-            //testin the unified 
+      , echo_async_divided: function() {
             var dfd = this.async(1000)
               , input = 'hello echo'
               // dfd.callback resolves the promise as long as no errors
               // are thrown from within the callback function
+              , callbacks = {
+                    callback: dfd.callback(function (result) {
+                        assert.equal(result, input, 'Result should be echod input.');
+                    })
+                  , errback: dfd.callback(function(error) {
+                    // this shouldn't run here ever
+                    throw error;
+                    })
+                }
+              ;
+            echo_job(callbacks, input);
+            // no need to return the promise; calling `async` makes the test async
+        }
+      , echo_async_unified: function() {
+            var dfd = this.async(1000)
+              , input = 'hello echo'
+              , callback = {
+                    unified: dfd.callback(function(error, result) {
+                        if(error)
+                            // this shouldn't run here ever
+                            throw error;
+                        else
+                            assert.equal(result, input, 'Result should be echod input.');
+                    })
+                }
+              ;
+            echo_job(callback, input);
+            // no need to return the promise; calling `async` makes the test async
+        }
+        , echo_async_promise: function() {
+            var dfd = this.async(1000)
+              , input = 'hello echo'
               , callback = dfd.callback(function (result) {
                     assert.equal(result, input, 'Result should be echod input.');
                 })
@@ -31,18 +62,47 @@ define([
                     throw error;
                 })
               ;
-            echo_job(true, input, callback, errback);
-            // no need to return the promise; calling `async` makes the test async
+            echo_job(true, input).then(callback, errback);
         }
-      , except_s: function() {
+      , except_sync: function() {
             var Err = function(){};
             assert.throws(exception_job.bind(null, false, Err), Err,
                                     undefined, 'Err should be raised.');
         }
-      , except_a: function() {
+      , except_async_divided: function() {
             var dfd = this.async(1000)
               , Err = function(){}
-              , callback = dfd.callback(function(result) {
+              , callbacks = {
+                    callback: dfd.callback(function(result) {
+                        // this shouldn't run here ever
+                        throw new Error('This shouldn\'t run here ever.');
+                    })
+                  , errback: dfd.callback(function(error) {
+                        assert.instanceOf(error, Err, 'Error should be an Err.');
+                    })
+                }
+              ;
+            exception_job(callbacks, Err);
+        }
+      , except_async_unified: function() {
+            var dfd = this.async(1000)
+              , Err = function(){}
+              , callback = {
+                    unified: dfd.callback(function(error, result) {
+                        if(error)
+                            assert.instanceOf(error, Err, 'Error should be an Err.');
+                        else
+                            // this shouldn't run here ever
+                            throw new Error('This shouldn\'t run here ever.');
+                    })
+                }
+              ;
+            exception_job(callback, Err);
+        }
+        , except_async_promise: function() {
+            var dfd = this.async(1000)
+              , Err = function(){}
+              , callback = dfd.callback(function (result) {
                     // this shouldn't run here ever
                     throw new Error('This shouldn\'t run here ever.');
                 })
@@ -50,7 +110,7 @@ define([
                     assert.instanceOf(error, Err, 'Error should be an Err.');
                 })
               ;
-            exception_job(true, Err, callback, errback);
+            exception_job(true, Err).then(callback, errback)
         }
     });
 });
